bd70e825d27d84fea5a45e9a7777fd99cceea665
Heluhan <m17501641586@gmail.com>
2023-06-20 13:12:47 +0800

    change to a working version

--- JS79/index.js
+++ JS79/index.js
@@ -1,28 +1,37 @@
 function add(items) {
-  //items [{name, price}]
-  //1.获取父节点
-  var tbody = document.getElementsByTagName("tbody")[0];
-  var tfoot = document.getElementsByTagName("tfoot")[0];
-  //2.获取初始数据
-  var num = tbody.children.length;
-  console.log(tfoot.innerText);
-  var total = parseFloat(tfoot.innerText.match(/\d+.\d+/)[0]);
-  // var total = tfoot.innerText.match(/^.*\t(\d*\.?\d*)\(.*\)$/)[1]
-  //3.创建子节点
-  //产品行
-  var tr = "";
-  for (let i in items) {
-    total = parseFloat((total + items[i].price).toFixed(2));
-    tr += `
-                    ${items[i].name}
-                    ${items[i].price.toFixed(2)}
-                    <a href="javascript:void(0);">删除</a>
-                   `;
-    num++;
+  const frag = document.createDocumentFragment();
+
+  for (let item of items) {
+    let newTr = document.createElement("tr");
+    newTr.innerHTML = `
+          <td>${item.name}</td>
+          <td>${item.price.toFixed(2)}</td>
+          <td><a href="javascript:void(0);">删除</a></td>
+      `;
+    frag.appendChild(newTr);
   }
-  //总计行
-  var tf = `总计${total.toFixed(2)}(${num}件商品)`;
-  //4.将新增的添加进去
-  tfoot.innerHTML = tf;
-  tbody.innerHTML = tbody.innerHTML + tr;
+
+  document.querySelector("tbody").appendChild(frag);
+  bind();
+}
+
+function bind() {
+  const tbody = document.querySelector("tbody");
+  let trs = [].slice.call(tbody.getElementsByTagName("tr"), 0);
+  let tfootData = document.querySelector("tfoot td");
+  let total = 0;
+  for (let tr of trs) {
+    let price = +tr.children[1].innerText;
+    total += price;
+    if (!tr.onclick) {
+      tr.onclick = function (e) {
+        if (e.target.tagName == "A") {
+          this.remove();
+          bind();
+        }
+      };
+    }
+  }
+  total = total.toFixed(2);
+  tfootData.innerText = `${total}(${trs.length}件商品)`;
 }
